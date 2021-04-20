41edf6400bb49606c654a304cfa4923e2afff109
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-20 17:50:18 +0900

    Flee and seek implemented.

---
+++ Assets/Bot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform target;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Flee(target.position);
+    }
+
+    void Seek(Vector3 targetPosition)
+    {
+        agent.SetDestination(targetPosition);
+    }
+
+    void Flee(Vector3 scary)
+    {
+        Vector3 location = transform.position;
+        Vector3 awayFrom = location - scary;
+        Vector3 targetLocation = location + awayFrom;
+
+        agent.SetDestination(targetLocation);
+    }
+}
