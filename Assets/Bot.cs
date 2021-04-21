c7269ae42d1e364a484a0c4f14fdafdd76dbb4ea
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-21 17:53:26 +0900

    Added pursue and botState.

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -7,7 +7,11 @@ public class Bot : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Transform target;
+    public BotState state;
     
+    private Vector3 toTarget => target.position - transform.position;
+    private bool targetIsBehind => Vector3.Dot(transform.forward, toTarget) < 0;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -15,12 +19,32 @@ public class Bot : MonoBehaviour
 
     void Update()
     {
-        Flee(target.position);
+        Behaviour();
     }
 
     void Seek(Vector3 targetPosition)
     {
         agent.SetDestination(targetPosition);
+        state = BotState.Seek;
+    }
+
+    void Behaviour()
+    {
+        if (target.GetComponent<Drive>().currentSpeed < 0.1f)
+        {
+            Seek(target.position);
+        }
+        else if (targetIsBehind)
+        {
+            Seek(target.position);
+        }
+        else
+        {
+            Pursue();
+        }
+        //pursue
+        //if target stopped moving, seek
+        //if target and self are heading the same direction, turn around and seek.
     }
 
     void Flee(Vector3 scary)
@@ -30,5 +54,23 @@ public class Bot : MonoBehaviour
         Vector3 targetLocation = location + awayFrom;
 
         agent.SetDestination(targetLocation);
+        state = BotState.Flee;
+    }
+
+    void Pursue()
+    {
+        Vector3 toTarget = target.position - transform.position;
+        float lookAheadDistance = toTarget.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Vector3 goal = target.position + lookAheadDistance * target.forward;
+        Seek(goal);
+        state = BotState.Pursue;
     }
 }
+
+public enum BotState
+{
+    Seek,
+    Pursue,
+    Flee
+}
+
